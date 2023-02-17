7956032ec9efa0e1ea25f8b9f6b275c6936d37cb
Sara Raley <sara.m.raley@gmail.com>
2023-02-16 19:18:34 -0700

    improves darkmode wigit

---
+++ script.js
@@ -0,0 +1,13 @@
+const options = {
+  bottom: "unset", // default: '32px'
+  right: "unset", // default: '32px'
+  left: "32px", // default: 'unset'
+  time: "0.5s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "#fff", // default: '#fff'
+  buttonColorDark: "#100f2c", // default: '#100f2c'
+  buttonColorLight: "#fff", // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: "🌙", // default: ''
+  autoMatchOsTheme: true, // default: true
+};
