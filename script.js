177d738295ae5f40cdf9fbda2ca3b90d3d296838
Sara Raley <sara.m.raley@gmail.com>
2023-03-21 14:50:17 -0600

    makes both search bars somewhat functional

--- script.js
+++ script.js
@@ -14,14 +14,30 @@ const options = {
 };
 // animal searchbar
 function search_animal() {
-  let input = document.getElementById("searchbar").value.toLowerCase();
-  document.getElementById("sanitized").innerHTML = input;
+  let input = document.getElementById("searchbar2").value;
+  input = input.toLowerCase();
   let x = document.getElementsByClassName("animals");
+
   for (i = 0; i < x.length; i++) {
     if (!x[i].innerHTML.toLowerCase().includes(input)) {
       x[i].style.display = "none";
     } else {
       x[i].style.display = "list-item";
+    }
+  }
+}
+// keyword searchbar
+function search_word() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("words");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+      // input.style.color = "red";
     }
   }
 }
